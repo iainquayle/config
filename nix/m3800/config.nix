ddceefdf29e2cf58665ac595f6686aeb95b01770
Iain Quayle <iainquayle@outlook.com>
2024-04-10 01:20:02 -0600

    m3800 hardware config, nvidia no work

---
+++ nix/m3800/config.nix
@@ -0,0 +1,40 @@
+{ config, pkgs, ... }:
+{
+  boot.kernelPackages = pkgs.linuxPackages_6_6; #6 1 hangs on shutdown
+
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.nvidia.acceptLicense = true;
+
+ /*
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+    #extraPackages = [ pkgs.amdvlk ];
+    #extraPackages32 = [ pkgs.driversi686Linux.amdvlk ];
+  };
+
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
+  };
+
+  #environment.systemPackages = with pkgs; [ ];
+
+  hardware.nvidia.prime = {
+    #sync.enable = true;
+    #offload = {
+    #  enable = true;
+    #  enableOffloadCmd = true;
+    #};
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:2:0:0";
+  };
+  */
+}
