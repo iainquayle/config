037d0643b9b202b5cbd0d9607ef86047bf8d2a76
iainquayle <iainquayle@outlook.com>
2024-05-22 21:43:43 -0600

    trim cuda env

--- nix/shells/cuda-fhs.nix
+++ nix/shells/cuda-fhs.nix
@@ -4,14 +4,16 @@
   targetPkgs = pkgs: with pkgs; [
     cudatoolkit
 	linuxPackages.nvidia_x11
-	stdenv.cc binutils 
-	libGL libGLU zlib
-	gnumake m4 util-linux
-
-	#python311Packages.torch
-	#python311Packages.pandas
+	stdenv.cc 
+	binutils 
+	libGL 
+	libGLU 
+	#zlib
+	#gnumake 
+	#m4 
+	util-linux
   ];
-  multiPkgs = pkgs: with pkgs; [zlib];
+  #multiPkgs = pkgs: with pkgs; [zlib];
   runScript = "zsh";
   profile = ''
 	export CUDA_PATH=${pkgs.cudatoolkit}
