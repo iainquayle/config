8e1baaf7ec3aa8ba3d314e34409b53d12a70c27b
iainquayle <iainquayle@outlook.com>
2024-05-27 19:48:14 -0600

    css eex

--- nvim/lua/core/init.lua
+++ nvim/lua/core/init.lua
@@ -24,3 +24,11 @@ vim.api.nvim_create_autocmd({'BufNewFile', 'BufRead',}, {
 	end,
 	group = generalSettingsGroup,
 })
+vim.api.nvim_create_autocmd({'BufNewFile', 'BufRead',}, {
+	pattern = {'*.css.eex'},
+	callback = function()
+		local buffer = vim.api.nvim_get_current_buf()
+		vim.api.nvim_buf_set_option(buffer, 'filetype', "css.eelixir")
+	end,
+	group = generalSettingsGroup,
+})
