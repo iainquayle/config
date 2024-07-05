f65db095cc21971a25ef1f29b042ac20ab31ce9b
iainquayle <iainquayle@outlook.com>
2024-07-05 17:48:43 -0400

    update

---
+++ nyxt/auto-config.3.lisp
@@ -0,0 +1,45 @@
+(define-configuration (input-buffer)
+  ((default-modes (pushnew 'nyxt/mode/vi:vi-normal-mode %slot-value%))))
+
+(define-configuration (prompt-buffer)
+  ((default-modes (pushnew 'nyxt/mode/vi:vi-insert-mode %slot-value%))))
+
+(define-configuration browser
+  ((theme theme:+dark-theme+)))
+
+(define-configuration (web-buffer)
+  ((default-modes (pushnew 'nyxt/mode/style:dark-mode %slot-value%))))
+
+(define-configuration (input-buffer)
+  ((default-modes (pushnew 'nyxt/mode/vi:vi-normal-mode %slot-value%))))
+
+(define-configuration (prompt-buffer)
+  ((default-modes (pushnew 'nyxt/mode/vi:vi-insert-mode %slot-value%))))
+
+(define-configuration (input-buffer)
+  ((default-modes (pushnew 'nyxt/mode/vi:vi-normal-mode %slot-value%))))
+
+(define-configuration (prompt-buffer)
+  ((default-modes (pushnew 'nyxt/mode/vi:vi-insert-mode %slot-value%))))
+
+(define-configuration browser
+  ((theme theme:+dark-theme+)))
+
+(define-configuration (web-buffer)
+  ((default-modes (pushnew 'nyxt/mode/style:dark-mode %slot-value%))))
+
+(define-configuration (web-buffer)
+  ((default-modes
+    (remove-if (lambda (nyxt::m) (string= (symbol-name nyxt::m) "DARK-MODE"))
+               %slot-value%))))
+
+(define-configuration (web-buffer)
+  ((default-modes (pushnew 'nyxt/mode/style:dark-mode %slot-value%))))
+
+(define-configuration (web-buffer)
+  ((default-modes
+    (remove-if (lambda (nyxt::m) (string= (symbol-name nyxt::m) "DARK-MODE"))
+               %slot-value%))))
+
+(define-configuration (web-buffer)
+  ((default-modes (pushnew 'nyxt/mode/style:dark-mode %slot-value%))))
