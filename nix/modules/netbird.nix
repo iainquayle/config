115545857736107c2133f59368af63c2f52aa39d
iainquayle <iainquayle@outlook.com>
2025-11-07 00:37:06 -0500

    working caddy reverse proxy

--- nix/modules/netbird.nix
+++ nix/modules/netbird.nix
@@ -3,14 +3,26 @@
     netbird = {
       enable = true;
     };
-    caddy = { 
+    caddy = { # these should be moved to machine specific configs
       enable = true; # may move the enable to its own?
-      virtualHosts."100.65.64.248:21338" = {
+      virtualHosts."http://strix.netbird.cloud:21339" = {
         extraConfig = ''
-          reverse_proxy 127.0.0.1:11434
+          reverse_proxy http://localhost:11434 {
+            header_up Host localhost:11434
+          }
+        '';
+      };
+      virtualHosts."http://strix.netbird.cloud:21338" = {
+        extraConfig = ''
+          respond "hello from strix" 
+        '';
+      };
+      virtualHosts."http://localhost:21338" = {
+        extraConfig = ''
+          respond "hello from local" 
         '';
       };
     };
   };
-  networking.firewall.allowedTCPPorts = [ 21338 ];
+  networking.firewall.allowedTCPPorts = [ 21338 21339 ];
 }
--- nix/modules/ollama.nix
+++ nix/modules/ollama.nix
@@ -1,16 +1,20 @@
 {...}: {
   #this is only available on local, can be changed but shouldnt
   #to get local and vpn, run a clear reverse nginx proxy that will bind to the vpn
-  services.ollama = {
-    enable = true;
-    acceleration = "cuda";
-    loadModels = [
-      #"deepseek-r1:14b"
-      #"qwen3:14b"
-      "mistral-small3.2:24b"
-      #"phi4-reasoning:plus"
-      "gpt-oss:20b"
-      #"devstral:24b"
-    ];
+  services = {
+    ollama = {
+      enable = true;
+      acceleration = "cuda";
+      port = 11434;
+      loadModels = [
+        #"deepseek-r1:14b"
+        #"qwen3:14b"
+        "mistral-small3.2:24b"
+        #"phi4-reasoning:plus"
+        "gpt-oss:20b"
+        #"devstral:24b"
+      ];
+    };
+    #nextjs-ollama-llm-ui.enable = true;
   };
 }
