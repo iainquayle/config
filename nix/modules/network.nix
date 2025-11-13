3225e53494e3e52d73bcc44b541a0e23adb1b80b
iainquayle <iainquayle@outlook.com>
2025-11-13 05:22:09 +0000

    samba notes

--- nix/modules/network.nix
+++ nix/modules/network.nix
@@ -16,6 +16,10 @@
     };
     useNetworkd = true; # replaces NetworkManager
     #useDHCP = false; # may be able to hand this off to networkd?
+    firewall = {
+      enable = true;
+      allowPing = false;
+    };
   };
   systemd.network = {
     enable = true;
@@ -29,17 +33,11 @@
   # Local link multicast name resolution, not the same as mdns
   services.resolved.llmnr = "false"; 
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
   # programs.mtr.enable = true;
   # programs.gnupg.agent = {
   #   enable = true;
   #   enableSSHSupport = true;
   # };
 
-  # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
-
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
 }
--- nix/modules/samba.nix
+++ nix/modules/samba.nix
@@ -7,13 +7,24 @@
       openFirewall = true;
       settings = {
         global = {
-          "workgroup" = "";
-          "server string" = "smbnix";
+          "workgroup" = "WORKGROUP";
+          "server string" = "file share";
           "netbios name" = "smbnix";
           "security" = "user";
-          "hosts allow" = "127.0.0.1 localhost"; # need to add machines netbird addr 
+          "hosts allow" = "strix.netbird.cloud 127.0.0.1 localhost"; # need to add machines netbird addr 
           "hosts deny" = "0.0.0.0/0";
-          "guest account" = "";
+          "guest account" = "smb-user";
+          "map to guest" = "bad user";
+        };
+        "public" = {
+          "path" = "/mnt/share/public";
+          "browsable" = "yes";
+          "read only" = "no";
+          "guest ok" = "yes";
+          "create mask" = "0644";
+          "directory mask" = "0775";
+          "force user" = "";
+          "force group" = "";
         };
       };
     };
@@ -22,4 +33,15 @@
     #  openFirewall = true;
     #};
   };
+  users = {
+    users."smb-user" = {
+      isSystemUser = true;
+      shell = "/sbin/nologin";
+      home = "";
+      group = "";
+    };
+    groups."smb-group" = {};
+  };
+  # either need to setup a auto systemd tempfile
+  # or, setup auto mount for a disk to a certain path
 }
