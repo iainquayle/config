fefb99445ff911ce7ca0e3511532c0b02bd1da36
iainquayle <iainquayle@outlook.com>
2025-12-31 13:48:49 -0500

    change color schemes

--- nvim/lua/plugins/gruvbox.lua
+++ nvim/lua/plugins/gruvbox.lua
@@ -1,11 +1,17 @@
 return {
-	"ellisonleao/gruvbox.nvim",
+
+	--"ellisonleao/gruvbox.nvim",
+	"sainnhe/gruvbox-material",
 	config = function ()
-		require("gruvbox").setup({
-			constrast = "soft",
-			transparent_mode = true,
-		})
-		vim.o.background = "dark"
-		vim.cmd([[colorscheme gruvbox]])
+		vim.g.gruvbox_material_palette = 'material'
+		vim.g.gruvbox_material_background = 'soft'
+		vim.g.gruvbox_material_transparent_background = 1
+		vim.cmd.colorscheme('gruvbox-material')
+
+		--require("gruvbox").setup({ constrast = "soft", transparent_mode = true, })
+		--vim.o.background = "dark"
+		--vim.cmd([[colorscheme gruvbox]])
+
+		vim.opt.termguicolors = true
 	end,
 }
